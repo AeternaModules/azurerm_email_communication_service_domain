56fb3a3c0efd6ceca7fa0ac3402685eba895c2bb
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:50:10 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "email_communication_service_domains" {
+  description = "All email_communication_service_domain resources"
+  value       = azurerm_email_communication_service_domain.email_communication_service_domains
+}
+
